368e0d33918317a5ebc50483582f9e06b5885ff2
David Grün <david.gruen.git@gmail.com>
2022-06-07 17:38:53 +0200

    update loop

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,9 @@ fn board_width(width: usize) -> usize {
     width + 3
 }
 
-fn board_reset(board: &mut Vec<u8>, width: usize, height: usize) {
+fn draw_border(board: &mut Vec<u8>, width: usize, height: usize) {
+    board.clear();
+    board.resize(board_height(height) * board_width(width), EMPTY);
     for w in 0..width {
         board[w + 1] = BORDER;
         board[(board_height(height) - 1) * board_width(width) + w + 1] = BORDER;
@@ -54,13 +56,40 @@ fn board_reset(board: &mut Vec<u8>, width: usize, height: usize) {
 fn draw_snake(board: &mut Vec<u8>, width: usize, snake: &LinkedList<(usize, usize)>) {
     let mut it = snake.into_iter();
     if let Some((h, w)) = it.next() {
-        board[h * width + w] = HEAD;
+        board[h * board_width(width) + w] = HEAD;
     } else {
         panic!("Snake has no head!");
     }
     while let Some((h, w)) = it.next() {
-        board[h * width + w] = BODY;
+        board[h * board_width(width) + w] = BODY;
     }
+}
+
+fn advance_snake(board: &mut Vec<u8>, width: usize, snake: &mut LinkedList<(usize, usize)>, direction: &(usize, usize)) -> (bool, bool) {
+    if let Some((h, w)) = snake.front() {
+        let new_head_h = h + direction.0;
+        let new_head_w = w + direction.1;
+
+        snake.push_front((new_head_h, new_head_w));
+
+        let out = match board[new_head_h * board_width(width) + new_head_w] {
+            FOOD => (true, false),
+            BORDER => (false, true),
+            BODY => (false, true),
+            EMPTY => (false, false),
+            _ => panic!("Impossible")
+        };
+        if !out.0 {
+            snake.pop_back();
+        }
+        out
+    } else {
+        panic!("Snake has no head!");
+    }
+}
+
+fn draw_food(board: &mut Vec<u8>, width: usize, food: &(usize, usize)) {
+    board[food.0 * board_width(width) + food.1] = FOOD;
 }
 
 fn random_free_spot(board: &Vec<u8>, width: usize) -> (usize, usize) {
@@ -87,31 +116,35 @@ fn main() {
     let width: usize = parse_arg(1);
     let height: usize = parse_arg(2);
 
-    let board_width = width + 2 + 1; // plus border plus newline
-    let board_height = height + 2; // plus border
-    let board_size = board_height * board_width;
+    let board_size = board_height(height) * board_width(width);
 
     let mut term = terminal::stdout();
 
     let mut board : Vec<u8> = std::vec::Vec::with_capacity(board_size);
-    board.resize(board_size, EMPTY);
 
     let mut food: (usize, usize);
     let mut snake: LinkedList<(usize, usize)> = LinkedList::new();
     let mut direction: (usize, usize) = (0, 1);
 
-    board_reset(&mut board, width, height);
-
+    draw_border(&mut board, width, height);
     snake.push_back((height / 2 + 1, width / 2 + 1));
-    draw_snake(&mut board, board_width, &snake);
-
-    food = random_free_spot(&board, board_width);
-    board[food.0 * board_width + food.1] = FOOD;
+    draw_snake(&mut board, width, &snake);
+    food = random_free_spot(&board, width);
+    draw_food(&mut board, width, &food);
 
     loop {
         std::thread::sleep(std::time::Duration::from_millis(1000));
-        
 
+        let (eaten, crashed) = advance_snake(&mut board, width, &mut snake, &direction);
+
+        draw_border(&mut board, width, height);
+        draw_snake(&mut board, width, &snake);
+        if eaten {
+            food = random_free_spot(&board, width);
+        }
+        draw_food(&mut board, width, &food);
+
+        // display
         if term.batch(Action::ClearTerminal(Clear::All)).is_err() ||
             term.batch(Action::MoveCursorTo(0, 0)).is_err() ||
             term.flush_batch().is_err() ||
@@ -119,7 +152,9 @@ fn main() {
             std::process::exit(1);
         }
 
+        if crashed {
+            break;
+        }
     }
-
 
 }
